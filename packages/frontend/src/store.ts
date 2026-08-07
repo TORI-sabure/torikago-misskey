5596c7b0b12e81119468137f1121e2539f960c99
TORI-sabure <torikagosuki.net@gmail.com>
2026-08-07 21:58:56 +0900

    fix(timeline): persist mutual timeline selection

--- packages/frontend/src/store.ts
+++ packages/frontend/src/store.ts
@@ -51,7 +51,7 @@ export const store = markRaw(new Pizzax('base', {
 	tl: {
 		where: 'deviceAccount',
 		default: {
-			src: 'home' as 'home' | 'local' | 'social' | 'global' | `list:${string}`,
+			src: 'home' as 'home' | 'mutual' | 'local' | 'social' | 'global' | `list:${string}`,
 			userList: null as Misskey.entities.UserList | null,
 			filter: {
 				withReplies: true,
