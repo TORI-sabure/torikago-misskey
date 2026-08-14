a05f9f9e281a8b5d288edd21a2323182603f19a6
TORI-sabure <torikagosuki.net@gmail.com>
2026-08-14 21:34:45 +0900

    fix(misskey-js): restore generated models.ts

--- packages/misskey-js/src/autogen/models.ts
+++ packages/misskey-js/src/autogen/models.ts
@@ -1,6 +1,3 @@
-Exit code: 0
-Wall time: 1 seconds
-Output:
 import { components } from './types.js';
 export type Error = components['schemas']['Error'];
 export type UserLite = components['schemas']['UserLite'];
