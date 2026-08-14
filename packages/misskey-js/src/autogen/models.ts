a982d3cef4e904c5603342e18962409f0f231b41
TORI-sabure <torikagosuki.net@gmail.com>
2026-08-14 21:36:11 +0900

    fix(misskey-js): restore complete generated models.ts

--- packages/misskey-js/src/autogen/models.ts
+++ packages/misskey-js/src/autogen/models.ts
@@ -69,4 +69,3 @@ export type ChatMessageLiteForRoom = components['schemas']['ChatMessageLiteForRo
 export type ChatRoom = components['schemas']['ChatRoom'];
 export type ChatRoomInvitation = components['schemas']['ChatRoomInvitation'];
 export type ChatRoomMembership = components['schemas']['ChatRoomMembership'];
-
