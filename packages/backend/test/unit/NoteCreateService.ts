868043b4956925868eeefdd38e93c38f85ccbda8
TORI-sabure <torikagosuki.net@gmail.com>
2026-08-22 00:56:25 +0900

    test: initialize moderator sensitive fixture

--- packages/backend/test/unit/NoteCreateService.ts
+++ packages/backend/test/unit/NoteCreateService.ts
@@ -96,6 +96,7 @@ describe('NoteCreateService', () => {
 			folderId: null,
 			folder: null,
 			isSensitive: false,
+			isSensitiveByModerator: false,
 			maybeSensitive: false,
 			maybePorn: false,
 			isLink: false,
