efc467138f32c1f0e1ed82a0a56fd7aac0273b5e
TORI-sabure <torikagosuki.net@gmail.com>
2026-08-22 01:34:37 +0900

    fix: add sensitive file column comment in migration

--- packages/backend/migration/1787327522966-add-sensitive-by-moderator-to-drive-file.js
+++ packages/backend/migration/1787327522966-add-sensitive-by-moderator-to-drive-file.js
@@ -8,6 +8,7 @@ export class AddSensitiveByModeratorToDriveFile1787327522966 {
 
     async up(queryRunner) {
         await queryRunner.query(`ALTER TABLE "drive_file" ADD "isSensitiveByModerator" boolean NOT NULL DEFAULT false`)
+        await queryRunner.query(`COMMENT ON COLUMN "drive_file"."isSensitiveByModerator" IS 'Whether the DriveFile was marked as sensitive by a moderator.'`)
     }
 
     async down(queryRunner) {
