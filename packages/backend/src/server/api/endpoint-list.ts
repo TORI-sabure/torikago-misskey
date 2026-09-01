02980b349acad6cca9e236bb422655abb6e0d3eb
TORI-sabure <torikagosuki.net@gmail.com>
2026-09-01 13:18:45 +0900

    feat: refine personalized recommended timeline

--- packages/backend/src/server/api/endpoint-list.ts
+++ packages/backend/src/server/api/endpoint-list.ts
@@ -342,6 +342,7 @@ export * as 'notes/reactions' from './endpoints/notes/reactions.js';
 export * as 'notes/reactions/create' from './endpoints/notes/reactions/create.js';
 export * as 'notes/reactions/delete' from './endpoints/notes/reactions/delete.js';
 export * as 'notes/recommended-timeline' from './endpoints/notes/recommended-timeline.js';
+export * as 'notes/recommended-timeline-has-new' from './endpoints/notes/recommended-timeline-has-new.js';
 export * as 'notes/renotes' from './endpoints/notes/renotes.js';
 export * as 'notes/replies' from './endpoints/notes/replies.js';
 export * as 'notes/search' from './endpoints/notes/search.js';
